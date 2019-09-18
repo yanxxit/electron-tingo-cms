8902f179881523a2fca2b61362b06389329e59f9
yanxxmh <xx.yan@tingo66.com>
2019-09-18 19:24:19 +0800

    a

--- src/main_update.js
+++ src/main_update.js
@@ -3,7 +3,8 @@ const log = require('electron-log');
 // 引入自动更新模块
 const { autoUpdater } = require("electron-updater");
 // const feedUrl = "https://github.com/shawflying/electron-tingo-cms/releases/download/untagged-a8ba4350679dfd8d625d/cms_setup_1.0.1.exe";
-const feedUrl = "https://github.com/shawflying/electron-tingo-cms/releases/download/untagged-a8ba4350679dfd8d625d/latest.yml";
+// const feedUrl = "https://github.com/shawflying/electron-tingo-cms/releases/download/untagged-a8ba4350679dfd8d625d/";
+const feedUrl = "http://127.0.0.1:8080";
 // if (require('electron-squirrel-startup')) return;
 
 autoUpdater.logger = log;
@@ -32,34 +33,19 @@ if (process.platform === 'darwin') {
 
 let mainWindow;
 
-function sendStatusToWindow(text) {
-  log.info("# sendStatusToWindow:", text);
-
-  mainWindow.webContents.send('message', text);
-}
-function createDefaultWindow() {
-  mainWindow = new BrowserWindow();
-
-  mainWindow.webContents.openDevTools();
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-  mainWindow.loadURL(`file://${__dirname}/update.html#v${app.getVersion()}`);
-  return mainWindow;
-};
-
-// 主进程监听渲染进程传来的信息
-ipcMain.on('update', (e, arg) => {
-  console.log("update");
-  checkForUpdates();
-});
 let checkForUpdates = () => {
   // 配置安装包远端服务器 
+  // autoUpdater.setFeedURL({
+  //   provider: "github",
+  //   repo: "electron-tingo-cms",
+  //   owner: "shawflying",
+  //   releaseType: "draft"
+  // });
   autoUpdater.setFeedURL(feedUrl);
-
+  log.info("# ==>", feedUrl, autoUpdater.getFeedURL())
   // 下面是自动更新的整个生命周期所发生的事件
   autoUpdater.on('error', function (message) {
-    sendUpdateMessage('error', message);
+    sendUpdateMessage('Error in auto-updater. ', message);
   });
   // 检查更新
   autoUpdater.on('checking-for-update', function (message) {
@@ -81,6 +67,7 @@ let checkForUpdates = () => {
     sendUpdateMessage('isUpdateNow');
     ipcMain.on('updateNow', (e, arg) => {
       autoUpdater.quitAndInstall();
+      mainWindow.destroy()
     });
   });
 
@@ -90,44 +77,27 @@ let checkForUpdates = () => {
 
 // 主进程主动发送消息给渲染进程函数
 function sendUpdateMessage(message, data) {
-  console.log({ message, data });
+  log.info("# sendUpdateMessage:", { message, data });
   mainWindow.webContents.send('message', { message, data });
 }
 
-autoUpdater.setFeedURL({
-  provider: "github",
-  owner: "shawflying",
-  repo: "election-tingo-cms"
-})
-
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  log.info("# message:", log_message)
-  sendStatusToWindow(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
+// 主进程监听渲染进程传来的信息
+ipcMain.on('update', (e, arg) => {
+  console.log("update");
+  checkForUpdates();
 });
+
 app.on('ready', function () {
-  // Create the Menu
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
   autoUpdater.checkForUpdatesAndNotify();
-  createDefaultWindow();
+  mainWindow = new BrowserWindow();
+
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+  mainWindow.loadURL(`file://${__dirname}/update.html#v${app.getVersion()}`);
 });
 app.on('window-all-closed', () => {
   app.quit();
